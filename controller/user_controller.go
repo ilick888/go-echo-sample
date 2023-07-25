872edcb869c4d98e8c878029f1fad9a874fd109d
ilick888 <ilick888@gmail.com>
2023-07-25 23:12:44 +0900

    CSRF対策

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -14,6 +14,7 @@ type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
+	CsrfToken(c echo.Context) error
 }
 
 type userController struct {
@@ -56,7 +57,7 @@ func (uc *userController) LogIn(c echo.Context) error {
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
 	cookie.HttpOnly = true
-	cookie.SameSite = http.SameSiteNoneMode
+	// cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
 
 	return c.NoContent(http.StatusOK)
@@ -70,7 +71,14 @@ func (uc *userController) LogOut(c echo.Context) error {
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
 	cookie.HttpOnly = true
-	cookie.SameSite = http.SameSiteNoneMode
+	// cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
 	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) CsrfToken(c echo.Context) error {
+	token := c.Get("csrf").(string)
+	return c.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
 }
